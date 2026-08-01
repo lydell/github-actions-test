a0a39d0a36e9a024a37bb20320fdbdabb1db2286
Simon Lydell <simon.lydell@gmail.com>
2026-08-01 16:19:43 +0200

    Communicate via pipe, then kill and close

--- child.js
+++ child.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const net = require('net');
+
+const pipeFilename = process.argv[2];
+
+const client = net.createConnection(pipeFilename);
+
+client.setEncoding('utf8');
+client.setNoDelay(true);
+
+client.on('data', function (msg) {
+  console.log('child got data:', msg);
+});
+
+client.write("Hello from client!\n");
--- parent.js
+++ parent.js
@@ -1,3 +1,32 @@
 const { fork } = require("child_process");
+const net = require('net');
+const readline = require('readline');
 
-fork('child.js');
+const pipeFilename = `/tmp/elm_test-${process.pid}.sock`;
+
+let child;
+const server = net.createServer((socket) => {
+  socket.setEncoding('utf8');
+  socket.setNoDelay(true);
+
+  // See the long note near client.write() in worker.js for why we do this.
+  // It fixes a nasty bug!
+  // https://nodejs.org/api/readline.html#example-read-file-stream-line-by-line
+  var stream = readline.createInterface({
+    input: socket,
+    crlfDelay: Infinity,
+  });
+
+  stream.on('line', function (data) {
+    console.log('parent got line:', data);
+    // socket.write('Reply from parent\n');
+    child.kill();
+    server.close();
+  });
+});
+
+server.on('listening', () => {
+  child = fork('child.js', [pipeFilename]);
+});
+
+server.listen(pipeFilename);
